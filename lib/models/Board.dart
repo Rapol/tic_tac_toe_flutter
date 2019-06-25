c9e724366d1cca5726287649f57d46d1a9e24a31
Rafael Pol <rafael.polabellas@sbdinc.com>
2019-06-25 08:30:14 -0400

    declaring Player enum, using enum to display Tile value, tracking currentPlayer

--- lib/models/Board.dart
+++ lib/models/Board.dart
@@ -1,41 +1,56 @@
 import 'package:flutter/foundation.dart';
 import 'dart:collection';
 
+enum Player {
+  X,
+  O,
+}
+
 class BoardModel extends ChangeNotifier {
   /// Internal, private state of the board
   final List<List<Square>> _squares = [
     [
-      Square(''),Square(''),Square(''),
+      Square(),Square(),Square(),
     ],
     [
-      Square(''),Square(''),Square(''),
+      Square(),Square(),Square(),
     ],
     [
-      Square(''),Square(''),Square(''),
+      Square(),Square(),Square(),
     ],
   ];
 
+  Player _currentPlayer = Player.X;
+
   // BoardModel() {}
-
-  void resetBoard() {
-
-  }
 
   UnmodifiableListView<List<Square>> get squares => UnmodifiableListView(_squares);
 
   UnmodifiableListView<Square> getRows(row) => UnmodifiableListView(squares[row]);
 
-  void setSquare(String value, row, column) {
-    this._squares[row].replaceRange(column, column + 1, [Square(value)]);
+  void setSquare(row, column) {
+    if(this._squares[row][column].value != null){
+      return;
+    }
+    this._squares[row].replaceRange(column, column + 1, [Square(_currentPlayer)]);
+    _nextTurn();
     notifyListeners();
+  }
+
+  void _nextTurn(){
+    _currentPlayer = _currentPlayer == Player.X ? Player.O : Player.X;
+  }
+
+  void resetBoard() {
+
   }
 }
 
 @immutable
 class Square {
-  final String _value;
+  final Player _value;
 
-  Square(this._value);
+  Square([this._value]);
 
-  String get value => this._value;  
+  Player get value => this._value;  
 }
--- lib/widget/Tile.dart
+++ lib/widget/Tile.dart
@@ -3,18 +3,30 @@ import 'package:provider/provider.dart';
 
 import 'package:tic_tac_toe/models/Board.dart';
 
+displayTileValue(Player p) {
+  switch(p){
+    case Player.X:
+      return 'X';
+    case Player.O:
+      return 'O';
+    default:
+      return '';
+  }
+}
+
 class Tile extends StatelessWidget {
   final int columnIndex;
   final int rowIndex;
   final Border borderSettings;
   final Square square;
 
-  Tile({ this.borderSettings, this.square, this.columnIndex, this.rowIndex });
+  Tile({this.borderSettings, this.square, this.columnIndex, this.rowIndex});
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () => Provider.of<BoardModel>(context, listen: false).setSquare('X', rowIndex, columnIndex),
+      onTap: () => Provider.of<BoardModel>(context, listen: false)
+          .setSquare(rowIndex, columnIndex),
       child: Column(
         children: <Widget>[
           Container(
@@ -26,7 +38,7 @@ class Tile extends StatelessWidget {
               padding: EdgeInsets.all(20.0),
               child: Center(
                 child: Text(
-                  square.value,
+                  displayTileValue(square.value),
                   style: TextStyle(
                     fontSize: 50.0,
                     color: Colors.indigo,
