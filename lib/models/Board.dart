c7d453308cfa5713dcdbdd6d5e48b24d9281709c
Rafael Pol <rafael.polabellas@sbdinc.com>
2019-06-25 09:26:27 -0400

    building board from playerMoves

--- lib/models/Board.dart
+++ lib/models/Board.dart
@@ -6,44 +6,60 @@ enum Player {
   O,
 }
 
-class BoardModel extends ChangeNotifier {
-  /// Internal, private state of the board
-  final List<List<Square>> _squares = [
-    [
-      Square(),Square(),Square(),
-    ],
-    [
-      Square(),Square(),Square(),
-    ],
-    [
-      Square(),Square(),Square(),
-    ],
-  ];
+int getTileNumber(int row, int column) => row * 3 + column;
 
+class BoardModel extends ChangeNotifier {
   Player _currentPlayer = Player.X;
+
+  final Map<Player, List<int>> _playerMoves = {
+    Player.X: [],
+    Player.O: [],
+  };
 
   // BoardModel() {}
 
-  UnmodifiableListView<List<Square>> get squares => UnmodifiableListView(_squares);
+  List<List<Square>> _buildBoard() {
+    return List<List<Square>>.generate(
+      3,
+      (int row) => List<Square>.generate(
+            3,
+            (int column) {
+              if (this
+                  ._playerMoves[Player.X]
+                  .contains(getTileNumber(row, column))) {
+                return Square(Player.X);
+              } else if (this
+                  ._playerMoves[Player.O]
+                  .contains(getTileNumber(row, column))) {
+                return Square(Player.O);
+              }
+              return Square();
+            },
+          ),
+    );
+  }
 
-  UnmodifiableListView<Square> getRows(row) => UnmodifiableListView(squares[row]);
+  UnmodifiableListView<List<Square>> get squares =>
+      UnmodifiableListView(_buildBoard());
+
+  UnmodifiableListView<Square> getRows(row) =>
+      UnmodifiableListView(squares[row]);
 
   void setSquare(row, column) {
-    if(this._squares[row][column].value != null){
+    if (this.squares[row][column].value != null) {
       return;
     }
-    this._squares[row].replaceRange(column, column + 1, [Square(_currentPlayer)]);
+    _playerMoves[_currentPlayer].add(getTileNumber(row, column));
+    print(_playerMoves);
     _nextTurn();
     notifyListeners();
   }
 
-  void _nextTurn(){
+  void _nextTurn() {
     _currentPlayer = _currentPlayer == Player.X ? Player.O : Player.X;
   }
 
-  void resetBoard() {
-
-  }
+  void resetBoard() {}
 }
 
 @immutable
@@ -52,5 +68,5 @@ class Square {
 
   Square([this._value]);
 
-  Player get value => this._value;  
+  Player get value => this._value;
 }
