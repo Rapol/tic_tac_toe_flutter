55deb04edc2e246bef436b8f55c136872463cd5d
Rafael Pol <rafael.polabellas@sbdinc.com>
2019-06-26 09:30:32 -0400

    uplifting state to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import 'package:tic_tac_toe/pages/BoardPage.dart';
+import 'package:tic_tac_toe/models/Board.dart';
+import 'package:tic_tac_toe/screens/GameScreen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: BoardPage(),
+    return ChangeNotifierProvider(
+      builder: (context) => BoardModel(),
+      child: MaterialApp(
+        title: 'Flutter Tic Tac Tow',
+        theme: ThemeData(
+          primarySwatch: Colors.yellow,
+          textTheme: TextTheme(
+            display4: TextStyle(
+              fontFamily: 'Corben',
+              fontWeight: FontWeight.w700,
+              fontSize: 24,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => GameScreen(),
+        },
+      ),
     );
   }
 }
---
+++ lib/screens/GameScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'package:tic_tac_toe/widget/Board.dart';
+
+class GameScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Board();
+  }
+}
